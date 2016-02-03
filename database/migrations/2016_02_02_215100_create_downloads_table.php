ac2decd96eb7673c98ce7122d8cc55e9a49f6e48
cakraamin <cakra.amin@yahoo.com>
2016-02-04 06:40:49 +0700

    tambahan migration download

---
+++ database/migrations/2016_02_02_215100_create_downloads_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDownloadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('downloads', function (Blueprint $table) {
+            $table->increments('id_download');
+            $table->string('nama_download',200);
+            $table->string('file_download',150);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('downloads');
+    }
+}
