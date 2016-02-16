3c10c0e18168a7c0c44a5bbf277f3200065f44bd
cakraamin <cakra.amin@yahoo.com>
2016-02-17 05:40:25 +0700

    tambahan database brand

---
+++ database/migrations/2016_02_16_222917_create_banners_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->increments('id_banner');
+            $table->string('judul_banner',100);
+            $table->string('gambar_banner',200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('banners');
+    }
+}
